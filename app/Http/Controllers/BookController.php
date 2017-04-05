b2478cad07c439740cd60e69977647084b62de9b
Maruf Hasan <marufhasanmunshi@gmail.com>
2017-04-06 00:10:16 +0600

    book aontroller done

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -17,9 +17,40 @@ class BookController extends Controller
 
     public function store()
     {
+        $user_id = Auth::user()->id; // current user id
+
         $book = new App\Book;
-        $author = new App\Author;
+        // $author = new App\Author;
         $rating = new App\Rating;
+        $purchase_date = new App\PurchaseDate;
+
+        $author = App\Author::firstOrNew([
+            'name' => request('name')
+        ]);
+
+        if ($author->id == null)
+        {
+            $author->save();
+        }
+
+        $book->category_id = request('category_id');
+        $book->edition = request('edition');
+        $book->image = request('image');
+        $book->language_id = request('language_id');
+        $book->pub_year = request('pub_year');
+        $book->title = request('title');
+        $book->save();
+
+        $author->books()->attach($book->id);
+        $book->users()->attach($user_id);
+
+        $purchase_date->book_id = $book->id;
+        $purchase_date->purchase_date = request('purchase_date');
+        $purchase_date->user_id = $uers_id;
+
+        $rating->book_id = $book->id;
+        $rating->user_id = $user_id;
+        $rating->value = request('value');
 
     }
 }
