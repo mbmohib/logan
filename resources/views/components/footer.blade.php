c43df7dcdb81cf25b16e0793e57dbafadab1586f
Mohammad Mohibbulllah <mbmohib@gmail.com>
2017-04-16 20:57:30 +0600

    Separeta registration

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,16 @@
+<section class="footer">
+
+    <div class="ui centered grid wow slideInUp">
+        <div class="row">
+			<div class="copyright">
+	            <a href="#">&copy; Maruf, Mohib, Momit</a>
+	        </div>
+        </div>
+
+
+
+    </div>
+
+    <a class="up-arrow" href="#home"><i class="arrow circle up icon"></i></a>
+
+</section>
---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,18 @@
+<div class="ui secondary inverted top large pointing menu">
+	<div class="left item">
+		<a href="#">
+			{{-- <i class="sidebar icon tablet"></i> --}}
+		</a>
+		<a href="#" class="logo">
+			<img src="/images/logo.png" alt="">
+			<span>BookShelf</span>
+		</a>
+	</div>
+
+	<div class="right item">
+		<a href="/" class="item">Home</a>
+		<a href="/#process" class="item">Process</a>
+		<a href="/#login" class="item">Login</a>
+		<a href="/registration" class="item">Registration</a>
+	</div>
+</div>
